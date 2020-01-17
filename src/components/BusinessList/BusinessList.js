3b1463ad57b96d52decb44da420fee37281ada84
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2020-01-17 15:30:41 +0000

    refactor business list to use list of businesses generated in App

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,14 +2,15 @@ import React from 'react'
 import Business from '../Business/Business'
 
 class BusinessList extends React.Component {
+
+  generateBusinesses() {
+    return this.props.businesses.map(business => <Business business={business}/>)
+  }
+
+
   render() {
     return <div className = "BusinessList">
-      <Business/>
-      <Business/>
-      <Business/>
-      <Business/>     
-      <Business/>
-      <Business/>
+      {this.generateBusinesses()}
     </div>
   }
 }
